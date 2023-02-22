6352b36fe6cf5f9a35a09591474175d53d1510ef
Noel Moon <nmoon778@gmail.com>
2023-02-22 15:15:36 -0500

    fixed cart button

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,5 +1,5 @@
 import CartIcon from "../Cart/CartIcon";
-import classes from "./HeaderCartButton.css";
+import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
   return (
