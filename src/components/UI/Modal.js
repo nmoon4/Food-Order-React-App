d4da8514642b635a5ba4ba8fe40c65fc45e4a2fb
Noel Moon <nmoon778@gmail.com>
2023-02-23 21:17:07 -0500

    Cart can now be opened and closed

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import classes from "./Modal.module.css";
 
 const Backdrop = (props) => {
-  return <div className={classes.backdrop} />;
+  return <div className={classes.backdrop} onClick={props.onClose} />;
 };
 
 const ModalOverlay = (props) => {
@@ -20,7 +20,10 @@ const Modal = (props) => {
   return (
     <Fragment>
       {/* Portal is used because the modal shows up in front everything else on the screen */}
-      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(
+        <Backdrop onClose={props.onClose} />,
+        portalElement
+      )}
       {ReactDOM.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
