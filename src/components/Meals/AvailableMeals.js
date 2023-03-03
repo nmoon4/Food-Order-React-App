a0c61028ed3961cb17ef685f5d40524fb9089cc5
Noel Moon <nmoon778@gmail.com>
2023-03-03 00:41:36 -0500

    Available meals are now fetched from the back-end

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 import Card from "./../UI/Card";
 import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
@@ -29,10 +31,42 @@ const DUMMY_MEALS = [
   },
 ];
 
-
 // This component displays all of the available meals
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([])
+
+  // If the fetchMeals() function is called without using useEffect(), it will create an infinite loop.
+  // This is because fetchMeals() modifies the state, which causes the component to re-render and call
+  // fetchMeals() again.
+
+  // The 2nd parameter of useEffect() is a list of dependencies. When a dependency is changed, 
+  // useEffect() will be called again. In this case, the dependencies are just an empty list
+  // because useEffect() only needs to be called once when the component is rendered for the first time.
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://react-http-692b0-default-rtdb.firebaseio.com/meals.json"
+      );
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+
+      setMeals(loadedMeals)
+    };
+
+    fetchMeals();
+  }, []);
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
